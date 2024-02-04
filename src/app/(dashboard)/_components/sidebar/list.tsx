939f7a97f30028f8b002c2c4e25c4042789b3489
KayChiken <kainov.dmitry370@yandex.ru>
2024-02-04 16:20:18 +0300

    dashboard, organizations, navbar, auth

---
+++ src/app/(dashboard)/_components/sidebar/list.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+
+import { useOrganizationList } from "@clerk/nextjs"
+import { Item } from "./item"
+
+
+
+export const List = () => {
+    const {userMemberships} = useOrganizationList({
+        userMemberships: {
+            infinite : true
+        }
+    })
+
+    if (!userMemberships.data?.length) return null
+    return (
+        <ul className="space-y-4">
+            {userMemberships.data?.map((member) => (
+                <p key={member.organization.id}>
+                    <Item key={member.organization.id} id={member.organization.name} name={member.organization.name} imageUrl={member.organization.imageUrl}/>
+                </p>
+            ))}
+        </ul>
+    )
+}
+
+
+export default List
---
+++ src/app/(dashboard)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div className='h-full flex-1 h-[calc(100%-80px)] p-6'>
+            Dashboard Root Page
+        </div>
+    );
+};
+
+export default Dashboard;
